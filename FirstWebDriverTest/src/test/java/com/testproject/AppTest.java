7ee46a31f8737681447253be941bf80ead708661
prasanta-sahoo <prasanta_sahoo@persistent.co.in>
2016-09-01 18:40:42 +0530

    1st commit

---
+++ FirstWebDriverTest/src/test/java/com/testproject/AppTest.java
@@ -0,0 +1,22 @@
+package com.testproject;
+
+
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+        System.out.println("APP Test");
+    }
+}
